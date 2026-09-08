ddc735dbdc2b4ee1ed6603404cc876bacdd5517b
beni-niyogisubizo <b.niyogisub@alustudent.com>
2026-09-09 00:22:46 +0200

    Complete React intro task 4 deployment

---
+++ react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import holbertonLogo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={holbertonLogo} alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        <label htmlFor="email">Email: </label>
+        <input id="email" type="email" />
+
+        <label htmlFor="password">Password: </label>
+        <input id="password" type="password" />
+
+        <button type="button">OK</button>
+      </div>
+
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
