442abba2b9d411c576f69a8bcbea2a3e0e264fdc
beni-niyogisubizo <b.niyogisub@alustudent.com>
2026-09-09 14:09:06 +0200

    Fix Webpack task 2 counter styling

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -6,7 +6,7 @@ $('body').append('<div id="logo"></div>');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append("<span id='count'></span>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
