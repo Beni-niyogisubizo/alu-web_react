c7e9e2f0854225cbd4f48054ee1799a624567bdd
beni-niyogisubizo <b.niyogisub@alustudent.com>
2026-09-09 14:52:51 +0200

    Fix Webpack task 2 CSS checker

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import '../css/main.css';
 import _ from 'lodash';
 
-$('body').append('<div id="logo"></div>');
+$('body').prepend('<div id="logo"></div>');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
